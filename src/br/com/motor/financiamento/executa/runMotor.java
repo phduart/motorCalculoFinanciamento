886108b6f9625aa7489cf0b625f29974e0f060f6
phduart <49646944+phduart@users.noreply.github.com>
2022-05-23 17:24:29 -0300

    ajustando codigo e novos calculos

--- src/br/com/motor/financiamento/executa/runMotor.java
+++ src/br/com/motor/financiamento/executa/runMotor.java
@@ -6,26 +6,29 @@ import java.math.RoundingMode;
 public class runMotor {
 
     public static void main(String[] args){
-        calcJurosDia();
+        calcIOF();
     }
 
-    private static void calcularCotaAmortizacao(){
+    private static BigDecimal calcularCotaAmortizacao(){
+        // receber valor de financiamento
         BigDecimal valorFinanciamento = new BigDecimal("10000");
+        // receber prazo de financiamento
         BigDecimal prazoFinancimento = new BigDecimal("360");
 
+        // CALCULO ---- valorFinaniamento/prazoFinaniamento
         BigDecimal cotaAmortizacao;
         cotaAmortizacao = valorFinanciamento.divide(prazoFinancimento, 2, RoundingMode.HALF_UP);
         System.out.println("Cota Amortizacao = " + cotaAmortizacao);
+
+        return cotaAmortizacao;
     }
 
     private static BigDecimal converterTaxaDeJurosMes(){
-        System.out.println("Convertendo Juros Mes - INICIO");
-        // receber numero inteiro e dividir por 100
+        // receber taxa -  numero inteiro e dividir por 100
         BigDecimal taxa = new BigDecimal("0.1");
 
         // potencia
         BigDecimal potencia = BigDecimal.ONE.divide(new BigDecimal("12"), 8, RoundingMode.FLOOR);
-        System.out.println("POTENCIA = " +  potencia); // tem que dar 0.083333
 
         taxa = BigDecimal.ONE.add(taxa).setScale(8, RoundingMode.FLOOR);
 
@@ -44,12 +47,11 @@ public class runMotor {
     }
 
     private static BigDecimal converterTaxaDeJurosDia(){
-        System.out.println("Convertendo Juros Dia - INICIO");
         // receber taxa
         BigDecimal taxa = converterTaxaDeJurosMes();
         taxa = taxa.divide(new BigDecimal("100"));
 
-        // potencia
+        // CALCULO ----
         BigDecimal potencia = BigDecimal.ONE.divide(new BigDecimal("30"), 8, RoundingMode.FLOOR);
         System.out.println("POTENCIA = " +  potencia); // tem que dar 0.03333
 
@@ -77,6 +79,7 @@ public class runMotor {
         BigDecimal potencia = BigDecimal.ONE.divide(new BigDecimal("12"), 8, RoundingMode.FLOOR);
         System.out.println("POTENCIA = " +  potencia); // tem que dar 0.083333
 
+        // CALCULO ----
         taxa = BigDecimal.ONE.add(taxa).setScale(8, RoundingMode.FLOOR);
 
         double taxaDouble = Double.parseDouble(taxa.toString());
@@ -97,30 +100,110 @@ public class runMotor {
     private static BigDecimal calcJurosMes(){
         // valorFinanciamento - Recebe
         BigDecimal valor = new BigDecimal("1000");
+
+        // processa - taxaJurosMes
         BigDecimal taxaMes = converterTaxaDeJurosMes();
+
+        // CALCULO --- valor * taxaMes
         BigDecimal somaJurosFinanciamento = valor.multiply(taxaMes).divide(new BigDecimal("100"));
         somaJurosFinanciamento = somaJurosFinanciamento.setScale(2, RoundingMode.FLOOR);
         System.out.println("Soma Juros/MES Financiamento = " + somaJurosFinanciamento);
+
         return somaJurosFinanciamento;
     }
 
     private static BigDecimal calcJurosDia(){
-        // diasCorridos = 10
-        // valorFananciamento - Recebe
-        BigDecimal valor = new BigDecimal("1500");
-        BigDecimal taxaDia = converterTaxaDeJurosDia().setScale(8, RoundingMode.FLOOR);
+        // receber - valorFananciamento
+        BigDecimal valorFananciamento = new BigDecimal("1500");
 
-        taxaDia = taxaDia.divide(new BigDecimal("100"));
-        taxaDia = taxaDia.add(BigDecimal.ONE).setScale(8, RoundingMode.FLOOR);
+        // processa - taxaJurosDia
+        BigDecimal taxaJurosDia = converterTaxaDeJurosDia().setScale(8, RoundingMode.FLOOR);
+
+        // CALCULO ----
+        taxaJurosDia = taxaJurosDia.divide(new BigDecimal("100"));
+        taxaJurosDia = taxaJurosDia.add(BigDecimal.ONE).setScale(8, RoundingMode.FLOOR);
         
-        double taxaDouble = Double.parseDouble(taxaDia.toString());
+        double taxaDouble = Double.parseDouble(taxaJurosDia.toString());
         double resultCalcTaxa = Math.pow(taxaDouble, 10); // 1.0023966
         System.out.println("TAXA DIAS ELEVADO A QUANTIDADE DE DIAS = " + resultCalcTaxa);
         BigDecimal taxaDecimal = new BigDecimal(resultCalcTaxa);
         taxaDecimal = taxaDecimal.subtract(BigDecimal.ONE).setScale(8, RoundingMode.HALF_UP);
 
-        BigDecimal somaJurosFinanciamento = valor.multiply(taxaDecimal).setScale(2, RoundingMode.HALF_UP);
+        BigDecimal somaJurosFinanciamento = valorFananciamento.multiply(taxaDecimal).setScale(2, RoundingMode.HALF_UP);
         System.out.println("Soma juros/DIA Financiamento = " + somaJurosFinanciamento);
         return null;
     }
+
+    private static BigDecimal calcRDM(){
+        // receber
+        BigDecimal prazoFinancimento = new BigDecimal("360");
+        // valor do juros da primeira parcela
+        BigDecimal valorJurosPrimeiraParcela = new BigDecimal("4897.25");
+
+        // CALCULO ----  valorJurosPrimeiraParcela / prazoFinancimento
+        BigDecimal valorRDM;
+        valorRDM = valorJurosPrimeiraParcela.divide(prazoFinancimento, 6, RoundingMode.FLOOR);
+        System.out.println("VALOR CALCULADO RDM = R$" + valorRDM);
+        return null;
+    }
+
+    private static BigDecimal calcCESH(){
+        //receber fator de carrecao CESH
+        BigDecimal fatorCorrecaoCESH = new BigDecimal("1.008");
+
+        // processa - dias corridos
+        BigDecimal diasCorridos = new BigDecimal("31");
+        // processa - valor seguro
+        BigDecimal valorSeguro = new BigDecimal("136.29");
+
+        //CALCULO ---- valorSeguro/(fatorCorrecaoCESH)^((12*diasCorridos)/365)
+        // x = (12*diasCorridos)
+        BigDecimal parteUm = new BigDecimal("12").multiply(diasCorridos);
+        System.out.println("x = (12*diasCorridos) = " + parteUm); // FAZENDO COM 31 DIAS = 372
+
+        // y = (x/365)
+        BigDecimal parteDois = parteUm.divide(new BigDecimal("365"), 8, RoundingMode.FLOOR);
+        System.out.println("y = (x/365) = " + parteDois); // FAZENDO COM 31 DIAS = 1.01917
+
+        // z = (fatorCorrecaoCESH ^ y)
+        Double fatorCorrecaoCESHDouble = Double.parseDouble(fatorCorrecaoCESH.toString());
+        Double parteDoisDouble = Double.parseDouble(parteDois.toString());
+        double parteTresDouble = Math.pow(fatorCorrecaoCESHDouble, parteDoisDouble);
+        System.out.println("(fatorCorrecaoCESH ^ y) =  " + parteTresDouble); // FAZENDO COM 31 DIAS = 1.008154
+
+        // (valorSeguro / z)
+        BigDecimal parteTres = new BigDecimal(parteTresDouble);
+        BigDecimal resultado = valorSeguro.divide(parteTres, 8, RoundingMode.FLOOR);
+        System.out.println("RESULTADO calcCESH = " + resultado);
+
+        return null;
+        // REPETIR PARA TODAS AS PARCELAS, APOS SOMAR VALOR TOTAL E DIVIDIR PELO FINANCIAMENTO
+    }
+
+    private static void calcIOF(){
+        // receber aliquota IOF diaria - 0.0082%
+        BigDecimal aliquotaIOFDiaria = new BigDecimal("0.0082").divide(new BigDecimal("100"));
+
+        // receber aliquota IOF adicional - 0.3800%
+        BigDecimal aliquotaIOFAdicional = new BigDecimal("0.3800").divide(new BigDecimal("100"));
+
+        // processa - dias corridos
+        BigDecimal diasCorridos = new BigDecimal("31");
+
+        // CALCULO ---- (diasCorridos * aliquotaIOFDiaria) + aliquotaIOFAdicional))* calcularCotaAmortizacao)
+        // x = (diasCorridos * aliquotaIOFDiaria)
+        BigDecimal parteUm = diasCorridos.multiply(aliquotaIOFDiaria);
+        System.out.println("x = (diasCorridos * aliquotaIOFDiaria) = " + parteUm);
+
+        // y = (x + aliquotaIOFAdicional)
+        BigDecimal parteDois = parteUm.add(aliquotaIOFAdicional);
+        System.out.println("y = (x + aliquotaIOFAdicional) = " + parteDois);
+
+        // y * calcularCotaAmortizacao
+        BigDecimal cotaAmortizacao = calcularCotaAmortizacao();
+        BigDecimal resultado = parteDois.multiply(cotaAmortizacao).setScale(2, RoundingMode.HALF_UP);
+        System.out.println("y * calcularCotaAmortizacao = " + resultado);
+
+    }
+
 }
